1a08af73ebb652d02b92a359be545022eda58578
jasviz03 <jasminvizcarra@Jasmins-MacBook-Air.local>
2026-01-23 23:42:10 -0800

    update

---
+++ 10demo.py
@@ -0,0 +1,29 @@
+# 10demo.py by jasmin ;P 
+print('hello, again') #greeting 
+
+"""
+This is a
+multi-line 
+comment hahaha
+"""
+
+#use a lot of whitespace; makes code look neater and provides clarity
+
+import math
+
+print(1.5e-2)
+
+print (1+2)
+
+#be sure to import math
+
+print(pow(2, 5))
+
+print(2**3) 
+
+print(pow (2, 3))
+
+print(math.pow(2, 3))
+
+print(math.log(2))
+
